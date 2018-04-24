ad7e5084d6fa4965ba9d649b76f2c1f83b7871cc
Ollie Chick <och26@uclive.ac.nz>
2018-04-25 00:24:17 +1200

    Add install_depencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python-pip python-dev build-essential 
+sudo pip install --upgrade pip 
+sudo pip install --upgrade virtualenv 
+sudo pip install pipenv
+pipenv install requests
