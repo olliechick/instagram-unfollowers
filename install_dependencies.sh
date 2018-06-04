c3403d3cff256805493b32ebecbc35b08e65cbc7
Ollie <och26@uclive.ac.nz>
2018-06-04 16:39:44 +1200

    Add InstagramApi to install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,3 +6,4 @@ sudo apt-get install python3-pip python-dev build-essential
 sudo pip3 install --upgrade pip 
 sudo pip3 install --upgrade virtualenv 
 sudo pip3 install requests
+sudo pip3 install InstagramApi --ignore-installed
